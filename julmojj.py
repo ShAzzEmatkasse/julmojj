91b1ef90092ce2265167dacc90b3eeed64b7c286
ShAzzEmatkasse <65095487+ShAzzEmatkasse@users.noreply.github.com>
2020-12-07 14:59:13 +0100

    jullmojj
    fick det inte att fungera funderar mer till nästa lektion

---
+++ julmojj.py
@@ -0,0 +1,9 @@
+x = input('Skriv ditt användarnamn:')
+print('Hej, ' + x)
+input('Vill du döda tomten Ja/Nej?') 
+    ja = "Du Dödade tomten inga julklpppar för dig"
+    nej = "du dädade inte tomten och får många julklappar"
+     if ja = ja:
+      print("Du dödade tomten :/ inga julklappar för dig")
+         elif nej = nej:
+              print("Du dödade inte tomten och får många julklappar")
